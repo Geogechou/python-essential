05937f394a22e9ce947aa8296c135e6bda61e16a
Geogechou <36061092+Geogechou@users.noreply.github.com>
2018-11-30 13:26:30 +0800

    Create 图片转bmp灰度图.py

---
+++ 图片转bmp灰度图.py
@@ -0,0 +1,12 @@
+"""
+
+图片转bmp灰度图
+
+"""
+import cv2
+imgpath = r"C:\Users\geoge\Desktop\2.bmp"
+img = cv2.imread(imgpath)
+cv2.imshow("Image",img)
+cv2.waitKey(0)
+img0 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imwrite("b.bmp",img0)
