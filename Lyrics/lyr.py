bdf273c76f48a06ae40c4fd86c87a1e41192f7c5
george <usasne@163.com>
2019-10-28 10:50:41 +0800

    同步spotify歌词软件,用dbus进行通信

---
+++ Lyrics/lyr.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import dbus
+import requests
+import json
+import time
+from show import match_time,refresh_console
+last_token = " "
+start_time = time.time()
+global_lyrics = ""
+# 传入lyrics和时间
+# def display(text,sec):
+
+def refresh():
+    session = dbus.SessionBus()
+    spotifydbus = session.get_object("org.mpris.MediaPlayer2.spotify","/org/mpris/MediaPlayer2")
+    spotifyinterface = dbus.Interface(spotifydbus, "org.freedesktop.DBus.Properties")
+    metadata = spotifyinterface.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    title = metadata['xesam:title']
+    artist = metadata['xesam:artist'][0]
+    imageUrl = metadata['mpris:artUrl']
+    global start_time
+    # 获取一个秒级别的时间戳
+    global global_lyrics
+    '''
+    if global_lyrics != "":
+        # 传递一个时间差
+        match_time(global_lyrics, time.time()-start_time)
+    '''
+    global last_token
+    if last_token == title+artist+imageUrl:
+        if global_lyrics != "":
+            refresh_console(time.time()-start_time)
+        return
+    match_time(global_lyrics, time.time()-start_time)
+    last_token = title+artist+imageUrl
+    start_time = time.time()
+    print(artist, "-", title)
+    # print(artist)
+    # print(metadata)
+
+    NEmusic_url = 'http://localhost:3000/search?keywords='+artist+" "+title
+    res = requests.get(NEmusic_url)
+    text = res.text
+    jr = json.loads(text)
+    try:
+        song_id = jr['result']['songs'][0]['id']
+        print("song id=", song_id)
+        NElyrics = 'http://localhost:3000/lyric?id='+str(song_id)
+        lyricsRes = requests.get(NElyrics)
+        lyricsJson = json.loads(lyricsRes.text)
+        # global global_lyrics
+        global_lyrics = lyricsJson['lrc']['lyric']
+        # print(global_lyrics)
+    except Exception as e:
+        print("网易云获取song失败", title, e)
+        global_lyrics = ""
+
+
+while True:
+    refresh()
---
+++ Lyrics/show.py
@@ -0,0 +1,31 @@
+import re
+import time
+
+update_time = time.time()
+data_list = []
+
+
+def refresh_console(start):
+    global update_time
+    global data_list
+    if time.time()-update_time < 1:
+        return
+    update_time = time.time()
+    i = 0
+    #print(data_list)
+    while i < len(data_list):
+        # 只更新未曾打印过的lyrics
+        if (abs(float(data_list[i][0])-start) < 2.0) and (data_list[i][2] == 0):
+            data_list[i][2] = 1
+            print(data_list[i][1])
+        i += 1
+
+
+# match the minute and second and lyrics text
+def match_time(txt, start):
+    p = re.compile("\[([\d]{1,2}):([\d]{1,2})[.\d]{1,}\](.*\n)")
+    match_list = p.findall(txt)
+    global data_list
+    for el in match_list:
+        data_list.append([int(el[0]) * 60 + int(el[1]), el[2], 0])
+    refresh_console(start)
