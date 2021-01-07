04e236fcb754ce53bb2b17b974660475f101bc62
georgezzzh <usasne@163.com>
2021-01-07 20:41:09 +0800

    散点图

--- 数据可视化/散点图/散点图_basic.py
+++ 数据可视化/散点图/散点图_basic.py
@@ -1,16 +1,13 @@
 """
 最简单的散点图
 """
-
-import matplotlib.pyplot as plt                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-
+import matplotlib.pyplot as plt 
 x_values=[1,2,3,4,5]
 y_values=[1,4,9,16,25]
-
 plt.scatter(x_values,y_values,s=20)
 # 设置图标标题并给坐标轴加上标签
 plt.title("Square number",fontsize=24)
-plt.xlabel("Value",fontsize=14)                                                                                                                                                                                                                                                                                                                          
+plt.xlabel("Value",fontsize=14) 
 plt.ylabel("Square of Value",fontsize=14)
 # 显示刻度标记的大小
 plt.tick_params(axis='both',labelsize=14)
